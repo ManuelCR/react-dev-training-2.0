c86cacd4b10d2c4d132ed5dacda76efe957f8960
ManuelCR92 <manuelcarrilloruiz@gmail.com>
2024-09-23 19:11:04 -0600

    calculator, digital clock

--- src/routes/Calculator.tsx
+++ src/routes/Calculator.tsx
@@ -1,38 +1,49 @@
 import React from 'react';
 
 import { Button } from "semantic-ui-react";
+import { useState } from 'react'; //Por que es algo que va a cambiar
 
 export const Calculator = () => {
+  //lo primero es usar el use state
+  const[result, setResult] = useState<string>("");// <> defines the type
+
+  const concatResult = (value: string) => {
+    setResult((prevResult) => {return prevResult + value});
+  }
+
+  const calculate = () => {
+    setResult((prevResult) => {return  eval(prevResult)});
+  }
 
   return (
     <>
 
       <div>
-        <input type="text" value="0" />
+        <input type="text" value={result} />
       </div>
       <div>
-        <Button>7</Button>
-        <Button>8</Button>
-        <Button>9</Button>
-        <Button>+</Button>
+        <Button onClick={() => concatResult("7")}>7</Button>
+        <Button onClick={() => concatResult("8")}>8</Button>
+        <Button onClick={() => concatResult("9")}>9</Button>
+        <Button onClick={() => concatResult("+")}>+</Button>
       </div>
       <div>
-        <Button>4</Button>
-        <Button>5</Button>
-        <Button>6</Button>
-        <Button>-</Button>
+        <Button onClick={() => concatResult("4")}>4</Button>
+        <Button onClick={() => concatResult("5")}>5</Button>
+        <Button onClick={() => concatResult("6")}>6</Button>
+        <Button onClick={() => concatResult("-")}>-</Button>
       </div>
       <div>
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-        <Button>*</Button>
+        <Button onClick={() => concatResult("1")}>1</Button>
+        <Button onClick={() => concatResult("2")}>2</Button>
+        <Button onClick={() => concatResult("3")}>3</Button>
+        <Button onClick={() => concatResult("*")}>*</Button>
       </div>
       <div>
-        <Button>0</Button>
-        <Button>.</Button>
-        <Button>=</Button>
-        <Button>/</Button>
+        <Button onClick={() => concatResult("0")}>0</Button>
+        <Button onClick={() => concatResult(".")}>.</Button>
+        <Button onClick={() => calculate()}>=</Button>
+        <Button onClick={() => concatResult("/")}>/</Button>
       </div>
 
     </>
--- src/routes/DigitalClock.tsx
+++ src/routes/DigitalClock.tsx
@@ -1,12 +1,28 @@
 
 import React from 'react';
+//To Do: que se actualice el tiempo
+import { useState, useEffect } from 'react';
+
 
 export const DigitalClock = () => {
 
+  const[time, useTime] = useState(new Date().toLocaleTimeString())// this is the state
+
+  const updateTime = () => {
+    setInterval(() => useTime(new Date().toLocaleTimeString()),1000)
+    
+  }
+  
+  //Para ejectuar solo cuando se monta por que si solo se manda a llamar updateTime se va a quedar colgando cada vez
+  useEffect(()=>{
+    const id:any = updateTime()// no es recomendable usar any, pero para el ejercicio lo dejamos en any, podemos usar window.setInterval y poner una variable para asignarle y usarla (no asignarla aquí solo llamar updateTime)
+    return () => {clearInterval(id)}
+  },[])
+  
   return (
     <div>
       <h1>Digital Clock</h1>
-      <h2>{new Date().toLocaleTimeString()}</h2>
+      <h2>{time}</h2>
     </div>
   );
 };
